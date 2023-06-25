2f1c3ed971a8c204c6ca0a99ab0c5977a1650791
mhgamble1 <mhgamble1@gmail.com>
2023-06-25 13:51:35 -0500

    Update create_sb.sql

--- create_db.sql
+++ create_db.sql
@@ -4,7 +4,15 @@ GRANT image_gallery TO postgres;
 CREATE DATABASE image_gallery OWNER image_gallery;
 \c image_gallery
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  username VARCHAR(50) NOT NULL,
-  password VARCHAR(50) NOT NULL
+  username VARCHAR(100) NOT NULL,
+  password VARCHAR(100),
+  full_name VARCHAR(200)
 );
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON users TO image_gallery;
+
+-- Grant privileges on existing sequences
+GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO image_gallery;
+
+-- Grant privileges on future sequences
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE ON SEQUENCES TO image_gallery;
